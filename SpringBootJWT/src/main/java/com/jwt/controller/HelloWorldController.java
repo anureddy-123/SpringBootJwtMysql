62281409f617637e6114ba8c8feb39f90653f430
anureddy-123 <sambiredd1999@gmail.com>
2021-05-21 19:19:55 +0530

    springbootjwtmysql

---
+++ SpringBootJWT/src/main/java/com/jwt/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.jwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public String firstPage() {
+		return "Hello World";
+	}
+	@RequestMapping("/hi")
+	public String secondPage() {
+		return "Hi..";
+	}
+
+}
---
+++ SpringBootJWT/src/main/java/com/jwt/service/JwtUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.jwt.service;
+
+import java.util.ArrayList;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.jwt.dao.UserDao;
+import com.jwt.model.DAOUser;
+import com.jwt.model.UserDTO;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		DAOUser user = userDao.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+	//save method to save the user details in the db,takes the details from UserDto object
+	public DAOUser save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return userDao.save(newUser);
+	}
+
+}
